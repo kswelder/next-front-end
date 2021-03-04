e8667f93183445cb0c8bdd03f979a85944000c04
kswelder <welderhayne2000@gmail.com>
2021-03-04 00:14:26 -0300

    Paginas de requisicao GET, POST, PUT e DELETE

---
+++ src/pages/requisicoes/page_delete.tsx
@@ -0,0 +1,18 @@
+import react from 'react'
+import Head from 'next/head'
+import axios from 'axios'
+
+export default function page_delete() {
+    return (
+        <div>
+            <Head>
+            <title>Create Next App</title>
+            <link rel="icon" href="/favicon.ico" />
+            </Head>
+            
+            <main>
+                DELETE
+            </main>
+        </div>
+    )
+}
---
+++ src/pages/requisicoes/page_get.tsx
@@ -0,0 +1,18 @@
+import react from 'react'
+import Head from 'next/head'
+import axios from 'axios'
+
+export default function page_get() {
+    return (
+        <div>
+            <Head>
+            <title>Create Next App</title>
+            <link rel="icon" href="/favicon.ico" />
+            </Head>
+            
+            <main>
+                GET
+            </main>
+        </div>
+    )
+}
